fc48807f0e15bd990b21c6e8930aecac70ae8274
WeijiaLinaSun <ws2406@nyu.edu>
2024-04-18 02:00:22 -0400

    finished this assignment!

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import subprocess
 import datetime
 
 from flask import Flask, render_template, request, redirect, url_for, make_response
-
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 # import logging
 # import sentry_sdk
 # from sentry_sdk.integrations.flask import (
@@ -41,7 +41,26 @@ load_dotenv(override=True)  # take environment variables from .env.
 
 # instantiate the app using sentry for debugging
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+users = {'11': {'password': '22'}}
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+    def __repr__(self):
+        return "%d" % self.id
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id in users:
+        return User(user_id)
+    return None
 # # turn on debugging if in development mode
 # app.debug = True if os.getenv("FLASK_ENV", "development") == "development" else False
 
@@ -65,12 +84,35 @@ except ConnectionFailure as e:
 
 
 @app.route("/")
+@login_required
 def home():
     """
     Route for the home page.
     Simply returns to the browser the content of the index.html file located in the templates folder.
     """
     return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            return '401'
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
 
 @app.route("/read")
 def read():
